e44dbdb05b7e10cbc909842fa33169c9f602a520
kotpavlik <kotpavlik88@gmail.com>
2024-08-03 23:52:52 +0300

    ton connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import type { Component } from 'solid-js';
+import { Router, Route } from "@solidjs/router";
+import MainScreen from './main_screen/MainScreen';
+import { NotFounded } from './404/NotFounded';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 
 const App: Component = () => {
   return (
-    <div class='w-screen h-screen relative flex items-center justify-center'>
-      Hello Pink Punk and fuck you
-      <div class='w-1 h-1'>
-        new div
-      </div>
-    </div>
+    <TonConnectUIProvider manifestUrl="https://<YOUR_APP_URL>/tonconnect-manifest.json">
+      <Router>
+        <Route path="/" component={MainScreen} />
+        <Route path="*404" component={NotFounded} />
+      </Router>
+    </TonConnectUIProvider>
   );
 };
 
--- src/main_screen/MainScreen.tsx
+++ src/main_screen/MainScreen.tsx
@@ -1,9 +1,14 @@
+
+import { TonConnectButton } from "@tonconnect/ui-react";
 import { Component } from "solid-js";
 
 const MainScreen: Component = () => {
 
+
     return (
         <div>
+            <span>My App with React UI</span>
+            <TonConnectButton className="my-button-class" />
 
         </div>
     )
