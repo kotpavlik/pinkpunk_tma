58a8f41b8043937d7e8bba299de2fd89c84d1abf
kotpavlik <kotpavlik88@gmail.com>
2024-08-04 02:06:11 +0300

    manifest

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App: Component = () => {
   return (
 
     <Router>
-      <Route path="/" component={MainScreen} />
+      <Route path="/pinkpunk_tma" component={MainScreen} />
       <Route path="*404" component={NotFounded} />
     </Router>
 
